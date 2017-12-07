b5f888d21199431fab1d2766ef0bac8010c29383
zekelongmit <31856002+zekelongmit@users.noreply.github.com>
2017-12-07 12:38:56 -0500

    Add files via upload

---
+++ final/fourth.js
@@ -0,0 +1,10 @@
+function setup() {
+	noStroke();
+	canvas = createCanvas(1200,525);
+	canvas.parent("thing");
+	background("0000ff");
+}
+function draw() {
+	fill("8DBDE8");
+	rect(0,0,width,height);
+}
